21c46174aaff910d001ff41260133e0e05ec5d69
Jérémie Caron <jeremiecaron06@gmail.com>
2026-03-06 12:05:36 -0500

    feat: Tab and shift tab now works in every mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -7,3 +7,10 @@ map("n", "<leader>q", ":q<CR>")
 map("n", "<leader>x", ":x<CR>")
 
 map("n", "<leader>h", ":nohlsearch<CR>")
+
+local opts = { noremap = true, silent = true }
+vim.keymap.set("n",    "<Tab>",         ">>",  opts)
+vim.keymap.set("n",    "<S-Tab>",       "<<",  opts)
+vim.keymap.set("v",    "<Tab>",         ">gv", opts)
+vim.keymap.set("v",    "<S-Tab>",       "<gv", opts)
+vim.keymap.set("i", "<S-Tab>", "<C-\\><C-N><<<C-\\><C-N>^i")
